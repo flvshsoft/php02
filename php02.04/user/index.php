6a2bce448255fc2c1fc09cffcc94ecbab5bf4200
Yura <yuhaya.lissera@gmail.com>
2025-02-19 15:04:15 +0700

    add: css

---
+++ php02.04/user/index.php
@@ -0,0 +1,56 @@
+<?php
+include '../koneksi.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="container mt-3">
+        <h2>Data User</h2>
+        <p>Ini adalah halaman tambah user</p>
+        <a href="tambah_form.php" class="btn btn-primary">Tambah User</a>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Username</th>
+                    <th>Password</th>
+                    <th>Role</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT * FROM users";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while ($row = $result->fetch_assoc()) {
+                ?>
+                        <tr>
+                            <td><?php echo $row["username"] ?></td>
+                            <td><?php echo $row["password"] ?></td>
+                            <td><?php echo $row["role"] ?></td>
+                        </tr>
+                <?php
+                    }
+                } else {
+                    echo "0 results";
+                }
+                $conn->close();
+                ?>
+
+            </tbody>
+        </table>
+    </div>
+
+</body>
+
+</html>
